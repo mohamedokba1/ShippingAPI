e60f6c37759221a9d1d4cd82a11371d54b0af14a
mohamedokba1 <47762482+mohamedokba1@users.noreply.github.com>
2023-05-27 01:30:10 +0300

    Add project files.

---
+++ Entities/Models/Trader.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shipping.Entities.Models;
+
+public class Trader
+{
+    [Key]
+    public Guid TraderId { get; set; }
+    [Required]
+    public string TraderName { get; set; } = string.Empty;
+}
