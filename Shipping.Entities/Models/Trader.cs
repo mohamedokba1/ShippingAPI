70b3aa185ed001b8a732dadedb114ea396944ab1
mohamedokba1 <47762482+mohamedokba1@users.noreply.github.com>
2023-05-29 23:13:00 +0300

    Entities Layer

--- Shipping.Entities/Models/Trader.cs
+++ Shipping.Entities/Models/Trader.cs
@@ -7,5 +7,17 @@ public class Trader
     [Key]
     public Guid TraderId { get; set; }
     [Required]
+    [StringLength(50)]
     public string TraderName { get; set; } = string.Empty;
+    [DataType(DataType.EmailAddress)]
+    public string? Email { get; set; }
+    [Required]
+    public string Address { get; set; } = string.Empty;
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+    public double CostPerRefusedOrder { get; set; }
+    public string? CompanyBranch { get; set; }
+    [DataType(DataType.PhoneNumber)]
+    public string ContactNumber { get; set; } = string.Empty;
 }
